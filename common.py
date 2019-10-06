e11a7917b0583f6779932000f431de32c9b465e7
Nima Chitsazan <nc2806@columbia.edu>
2019-10-06 18:26:56 -0400

    spatial features

--- common.py
+++ common.py
@@ -5,5 +5,5 @@ Created on Fri Sep 27 11:26:39 2019
 @author: rmahajan14
 """
 
-DATA_DIR = r'/mnt/d/DiDi_Ride_Data'
-CACHE_DIR = r'/mnt/d/DiDi_Ride_Data/Ride_Data_Cache'
+DATA_DIR = r'../../data'
+CACHE_DIR = r'../../data/Ride_Data_Cache'
